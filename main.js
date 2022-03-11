6f99b590faf995814e7593edf353eb48d9956215
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2022-03-10 19:44:24 -0500

    example fonctionnel

--- main.js
+++ main.js
@@ -1,14 +1,73 @@
-/// Canvas capture demo
+var elementToRecord = document.getElementById('element-to-record');
+var canvas2d = document.getElementById('background-canvas');
+var context = canvas2d.getContext('2d');
 
-let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-let recorder = new RecordRTCPromisesHandler(stream, {
-    type: 'video'
+canvas2d.width = elementToRecord.clientWidth;
+canvas2d.height = elementToRecord.clientHeight;
+
+var isRecordingStarted = false;
+var isStoppedRecording = false;
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+(function looper() {
+    if(!isRecordingStarted) {
+        return setTimeout(looper, 500);
+    }
+
+    html2canvas(elementToRecord).then(function(canvas) {
+        context.clearRect(0, 0, canvas2d.width, canvas2d.height);
+        context.drawImage(canvas, 0, 0, canvas2d.width, canvas2d.height);
+
+        if(isStoppedRecording) {
+            return;
+        }
+
+        requestAnimationFrame(looper);
+    });
+})();
+
+var recorder = new RecordRTC(canvas2d, {
+    type: 'canvas'
 });
-recorder.startRecording();
 
-const sleep = m => new Promise(r => setTimeout(r, m));
-await sleep(3000);
+document.getElementById('btn-start-recording').onclick = function() {
+    this.disabled = true;
+    
+    isStoppedRecording =false;
+    isRecordingStarted = true;
 
-await recorder.stopRecording();
-let blob = await recorder.getBlob();
-invokeSaveAsDialog(blob);
+    recorder.startRecording();
+    document.getElementById('btn-stop-recording').disabled = false;
+};
+
+document.getElementById('btn-stop-recording').onclick = function() {
+    this.disabled = true;
+    
+    recorder.stopRecording(function() {
+        isRecordingStarted = false;
+        isStoppedRecording = true;
+
+        var blob = recorder.getBlob();
+        // document.getElementById('preview-video').srcObject = null;
+        document.getElementById('preview-video').src = URL.createObjectURL(blob);
+        document.getElementById('preview-video').parentNode.style.display = 'block';
+        elementToRecord.style.display = 'none';
+
+        // window.open(URL.createObjectURL(blob));
+    });
+};
+
+var timer = document.getElementById('timer');
+var counter = document.getElementById('counter');
+var interval = setInterval(function() {
+    timer.innerHTML = new Date();
+    counter.innerHTML = (Math.random() * 100).toString().replace('.', '');
+}, 1000);
